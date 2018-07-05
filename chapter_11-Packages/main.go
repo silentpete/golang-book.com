0018922cf27e4f162f4b48beefd3ff0e2ef15174
Peter Gallerani <peter.gallerani@gmail.com>
2018-07-05 14:03:47 -0600

    eighth commit

---
+++ chapter_11-Packages/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"./math"
+	"./say"
+)
+
+func main() {
+	xs := []float64{1, 2, 3, 4}
+	avg := math.Average(xs)
+	fmt.Println(avg)
+	say.Durp("peter")
+}
---
+++ chapter_11-Packages/say/say.go
@@ -0,0 +1,9 @@
+// Package say is used for printing out things done to strings.
+package say
+
+import "fmt"
+
+// Durp prints out a durped string passed in.
+func Durp(s string) {
+	fmt.Println("Durp ->", s)
+}
